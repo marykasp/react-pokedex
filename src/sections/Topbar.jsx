5f0dc2c3f65efa1ac4dae2e138d401594a46fc5d
marykasp <kasparian.mary@gmail.com>
2023-11-19 10:08:03 -0600

    style: css reset and style topbar

--- src/sections/Topbar.jsx
+++ src/sections/Topbar.jsx
@@ -7,21 +7,23 @@ const Topbar = () => {
     <div className="title">
       <div className="title__left">
         <p>Pokedex</p>
-        <div className="caught">
-          <img
-            src={pokeballcolor}
-            alt="pokemon"
-            style={{ width: "30px", marginRight: "10px" }}
-          />
-          <p>438</p>
-        </div>
-        <div className="seen">
-          <img
-            src={pokeball}
-            alt=""
-            style={{ width: "40px", marginRight: "10px" }}
-          />
-          <p>649</p>
+        <div className="caught-seen">
+          <div className="caught">
+            <img
+              src={pokeballcolor}
+              alt="pokemon"
+              style={{ width: "30px", marginRight: "10px" }}
+            />
+            <p>438</p>
+          </div>
+          <div className="seen">
+            <img
+              src={pokeball}
+              alt=""
+              style={{ width: "30px", marginRight: "10px" }}
+            />
+            <p>649</p>
+          </div>
         </div>
       </div>
       <p style={{ color: "white" }}>A -&gt; Z</p>
